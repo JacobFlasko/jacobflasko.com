b004ae884541721d7c164589eb4266f139d53fa9
Jacob <jrf21@students.ptcollege.edu>
2024-03-13 22:01:54 -0400

    Add Hero Text

--- personal-website/app/components/Other/MainPageHeroText/MainPageHeroText.tsx
+++ personal-website/app/components/Other/MainPageHeroText/MainPageHeroText.tsx
@@ -1,26 +1,61 @@
-// Add "use client" at the top of the file to mark this as a Client Component
 "use client";
 
 import React, { useEffect, useState } from "react";
 
-const rotatingTerms = ["incredible", "memorable", "eye-catching"];
+const rotatingTerms = [
+  "Incredible",
+  "Memorable",
+  "Eye-catching",
+  "Remarkable",
+  "Extraordinary",
+];
 
 const MainPageHeroText: React.FC = () => {
   const [currentTerm, setCurrentTerm] = useState(0);
+  const [isEntering, setIsEntering] = useState(true); // Simplify the state to a boolean for entering/exiting
 
   useEffect(() => {
-    const intervalId = setInterval(() => {
-      setCurrentTerm((currentTerm) => (currentTerm + 1) % rotatingTerms.length);
-    }, 2000); // Change the text every 2000 milliseconds (2 seconds)
+    // Set the text to enter on initial render
+    setIsEntering(true);
 
-    return () => clearInterval(intervalId); // Cleanup the interval on component unmount
+    const intervalId = setInterval(() => {
+      // Trigger exit animation
+      setIsEntering(false);
+
+      // Change term and restart entering animation after exit animation completes
+      setTimeout(() => {
+        setCurrentTerm((prevTerm) => (prevTerm + 1) % rotatingTerms.length);
+        setIsEntering(true);
+      }, 500); // Matches the duration of the exit animation
+    }, 3000); // Total duration before changing the term, including animation time
+
+    return () => clearInterval(intervalId);
   }, []);
 
+  // Define styles for entering and exiting states
+  const enteringStyle = {
+    opacity: 1,
+  };
+  const exitingStyle = {
+    opacity: 0,
+  };
+
+  // Apply different styles based on the entering/exiting state
+  const dynamicWordStyle = isEntering ? enteringStyle : exitingStyle;
+
   return (
-    <div className="text-center">
-      <p>
+    <div
+      className="text-center relative overflow-hidden"
+      style={{ height: "24px" }}
+    >
+      <p style={{ position: "absolute", width: "100%" }}>
         Let's create something{" "}
-        <span className="text-primary">{rotatingTerms[currentTerm]}</span>
+        <span
+          className="text-primary"
+          style={{ ...dynamicWordStyle, transition: "opacity 500ms ease" }}
+        >
+          {rotatingTerms[currentTerm]}
+        </span>
       </p>
     </div>
   );
