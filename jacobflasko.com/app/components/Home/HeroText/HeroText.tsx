c76368f21bfa6ab0fbc0478e36e63546a152eeb9
Jacob <jrf21@students.ptcollege.edu>
2024-03-15 19:48:30 -0400

    Add HeroText

---
+++ jacobflasko.com/app/components/Home/HeroText/HeroText.tsx
@@ -0,0 +1,68 @@
+"use client";
+import React, { useEffect, useState } from "react";
+
+const rotatingTerms = [
+  "Incredible",
+  "Innovative",
+  "Groundbreaking",
+  "World-Changing",
+  "Memorable",
+];
+
+const HeroText: React.FC = () => {
+  const [currentTerm, setCurrentTerm] = useState(0);
+  const [isEntering, setIsEntering] = useState(true); // Simplify the state to a boolean for entering/exiting
+
+  useEffect(() => {
+    // Set the text to enter on initial render
+    setIsEntering(true);
+
+    const intervalId = setInterval(() => {
+      // Trigger exit animation
+      setIsEntering(false);
+
+      // Change term and restart entering animation after exit animation completes
+      setTimeout(() => {
+        setCurrentTerm((prevTerm) => (prevTerm + 1) % rotatingTerms.length);
+        setIsEntering(true);
+      }, 500); // Matches the duration of the exit animation
+    }, 3000); // Total duration before changing the term, including animation time
+
+    return () => clearInterval(intervalId);
+  }, []);
+
+  // Define styles for entering and exiting states
+  const enteringStyle = {
+    opacity: 1,
+  };
+  const exitingStyle = {
+    opacity: 0,
+  };
+
+  // Apply different styles based on the entering/exiting state
+  const dynamicWordStyle = isEntering ? enteringStyle : exitingStyle;
+
+  return (
+    <div className="text-center relative" style={{ height: "24px" }}>
+      <p
+        style={{
+          display: "inline-block",
+        }}
+        className="text-xl md:text-4xl lg:text-5xl whitespace-nowrap"
+      >
+        Let's create something{" "}
+        <span
+          className="text-primary"
+          style={{
+            ...dynamicWordStyle,
+            transition: "opacity 500ms ease",
+          }}
+        >
+          {rotatingTerms[currentTerm]}
+        </span>
+      </p>
+    </div>
+  );
+};
+
+export default HeroText;
--- jacobflasko.com/app/page.tsx
+++ jacobflasko.com/app/page.tsx
@@ -1,8 +1,17 @@
 import React from "react";
 import Navbar from "./components/global/Navbar/Navbar";
+import HeroText from "./components/Home/HeroText/HeroText";
 
 const page = () => {
-  return <Navbar />;
+  return (
+    <>
+      <Navbar />
+      {/* First Section for hero text and icons */}
+      <section className="flex h-screen w-full justify-center items-center">
+        <HeroText />
+      </section>
+    </>
+  );
 };
 
 export default page;
