3d7d59a02ca7d981fa8d0e7e9e42adc441519179
Jacob <jrf21@students.ptcollege.edu>
2024-03-15 21:28:03 -0400

    Finish Hero Layout

--- jacobflasko.com/app/page.tsx
+++ jacobflasko.com/app/page.tsx
@@ -2,6 +2,10 @@ import React from "react";
 import Navbar from "./components/global/Navbar/Navbar";
 import HeroText from "./components/Home/HeroText/HeroText";
 import Image from "next/image";
+//CSS Style Import
+import styles from "./styles.module.css";
+//Images
+import { FaReact } from "react-icons/fa";
 
 const page = () => {
   return (
@@ -11,49 +15,49 @@ const page = () => {
       <section className="flex flex-col space-y-24 lg:space-y-36 h-screen w-full justify-center items-center snap-center">
         <div className="flex space-x-16 md:space-x-32 lg:space-x-48 xl:space-x-72">
           <Image
-            src=""
+            src="/Home/javascriptIcon.png"
             width={175}
             height={175}
             alt="Javascript Icon"
-            className="shadow-2xl shadow-yellow-300"
+            className={`shadow-2xl shadow-yellow-300 -rotate-12 ${styles.bump}`}
           />
           <Image
-            src=""
+            src="/Home/github_icon.png"
             width={175}
             height={175}
-            alt="Javascript Icon"
-            className="shadow-2xl shadow-red-500"
+            alt="Github Icon"
+            className={`shadow-2xl shadow-red-500  ${styles.bump_delay}`}
           />
           <Image
-            src=""
+            src="/Home/react_icon.png"
             width={175}
             height={175}
-            alt="Javascript Icon"
-            className="shadow-2xl shadow-blue-300"
+            alt="React Icon"
+            className={`shadow-2xl shadow-blue-300 rotate-12 ${styles.bump}`}
           />
         </div>
         <HeroText />
         <div className="flex space-x-16 md:space-x-32 lg:space-x-48 xl:space-x-72">
           <Image
-            src="/Home/next.svg"
+            src=""
             width={175}
             height={175}
-            alt="Javascript Icon"
-            className="shadow-2xl shadow-red-300"
+            alt="HTML 5 Icon"
+            className={`shadow-2xl shadow-red-300 rotate-12 ${styles.bump_delay}`}
           />
           <Image
             src=""
             width={175}
             height={175}
-            alt="Javascript Icon"
-            className="shadow-2xl shadow-yellow-300"
+            alt="Python Icon"
+            className={`shadow-2xl shadow-yellow-300  ${styles.bump}`}
           />
           <Image
             src=""
             width={175}
             height={175}
-            alt="Javascript Icon"
-            className="shadow-2xl shadow-yellow-300"
+            alt="NextJS Icon"
+            className={`shadow-2xl shadow-black -rotate-12 ${styles.bump_delay}`}
           />
         </div>
       </section>
