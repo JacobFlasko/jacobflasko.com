15aae366ace11c0f14d4554fb1c825c0c678f2a5
Jacob <jrf21@students.ptcollege.edu>
2024-03-18 19:07:42 -0400

    Enhance Skill Card

--- jacobflasko.com/app/components/Skills/Card/SkillCard.tsx
+++ jacobflasko.com/app/components/Skills/Card/SkillCard.tsx
@@ -8,7 +8,7 @@ interface SkillCardProps {
 
 const SkillCard: React.FC<SkillCardProps> = ({ iconSrc, text }) => {
   return (
-    <div className="skill-card w-80 h-80 lg:w-80 bg-gray-200 shadow-lg hover:scale-110 hover:shadow-xl duration-500 rounded-3xl m-4">
+    <div className="skill-card w-80 h-80 lg:w-80 bg-gray-200 shadow-lg hover:scale-110 hover:shadow-xl duration-500 rounded-3xl m-4 flex flex-col items-center justify-center">
       <div className="aspect-w-1 aspect-h-1">
         <Image
           src={iconSrc}
