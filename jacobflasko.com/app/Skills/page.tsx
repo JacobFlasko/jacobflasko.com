d8f20a70df25488d8f2d4a58039e4f1469a053a2
Jacob <jrf21@students.ptcollege.edu>
2024-03-18 19:23:04 -0400

    Finish Top Row of Skills Page

--- jacobflasko.com/app/Skills/page.tsx
+++ jacobflasko.com/app/Skills/page.tsx
@@ -18,10 +18,30 @@ const page = () => {
       <section className="flex-grow flex flex-col items-center space-y-2">
         <SkillsHeader />
         <div className="flex flex-wrap justify-around w-full pt-10">
-          <SkillCard iconSrc={""} text={"Base Text"} />
-          <SkillCard iconSrc={""} text={"Hello There"} />
-          <SkillCard iconSrc={""} text={"Hello There"} />
-          <SkillCard iconSrc={""} text={"Hello There"} />
+          <SkillCard
+            iconSrc={"/Skills/java_icon.png"}
+            text={
+              "Java was the first programming language I studied in school. It was fundamental in teaching me software development and it still holds a special place in my heart."
+            }
+          />
+          <SkillCard
+            iconSrc={"/Skills/python-logo-only.png"}
+            text={
+              'Python served as my introduction to programming, the first line of code I ever wrote was "Hello World." I still practice and utilize python regularly.'
+            }
+          />
+          <SkillCard
+            iconSrc={"/Skills/Csharp_Logo.png"}
+            text={
+              "If I have to, I will. That is my outlook on writing C#. Although it is very similar to java, the errors I am thrown look nothing like Java. Still, I recognize that C# is a very powerful language and it boasts many opportunities."
+            }
+          />
+          <SkillCard
+            iconSrc={"/Skills/javascriptIcon.png"}
+            text={
+              "I would consider Javascript and its derivitives to be my preferred programming language(s). On a day to day basis I believe that Javascript is my most used programming language."
+            }
+          />
         </div>
         <div className="flex flex-wrap justify-around w-full pt-10">
           <SkillCard iconSrc={""} text={"Base Text"} />
--- jacobflasko.com/app/components/Skills/Card/SkillCard.tsx
+++ jacobflasko.com/app/components/Skills/Card/SkillCard.tsx
@@ -8,12 +8,14 @@ interface SkillCardProps {
 
 const SkillCard: React.FC<SkillCardProps> = ({ iconSrc, text }) => {
   return (
-    <div className="skill-card w-80 h-80 lg:w-80 bg-gray-200 shadow-lg hover:scale-110 hover:shadow-xl duration-500 rounded-3xl m-4 flex flex-col items-center justify-center">
+    <div className="skill-card w-80 h-80 lg:w-80 bg-gray-100 shadow-xl hover:scale-110 hover:shadow-xl duration-500 rounded-3xl m-4 flex flex-col items-center justify-center">
       <div className="aspect-w-1 aspect-h-1">
         <Image
           src={iconSrc}
+          width={75}
+          height={75}
           alt="Skill Icon"
-          className="object-cover rounded-t-3xl"
+          className="object-cover drop-shadow-xl"
         />
       </div>
       <div className="p-4">
